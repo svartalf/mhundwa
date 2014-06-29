e6eeeb5c53aa2336a861ae66305e0edd6386e352
svartalf <self@svartalf.info>
2014-06-29 11:19:18 +0900

    Скрипт для деплоя на сервер

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from fabric.api import env, sudo
+
+import settings
+
+
+HOSTS = getattr(settings, 'DEPLOY_HOSTS', ())
+
+PROJECT_ROOT = getattr(settings, 'DEPLOY_ROOT', '/var/www/mhundwa')
+ENV_FOLDER = getattr(settings, 'DEPLOY_ENV_FOLDER', '.env')
+
+# Список доменов для подключения
+env.hosts = HOSTS
+
+
+def deploy():
+    sudo('cd {}; git pull'.format(PROJECT_ROOT))
+
+    sudo('{} -r {}'.format(
+        os.path.join(PROJECT_ROOT, ENV_FOLDER, 'bin/pip'),
+        os.path.join(PROJECT_ROOT, 'requirements.txt'),
+    ))
